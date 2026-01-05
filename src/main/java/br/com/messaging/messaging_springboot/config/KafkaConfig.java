9eec92ecdc17b38d6d3058db200c9f81fe273c5e
PedroLucenaAlves <alvespedroalonso@gmail.com>
2026-01-05 20:40:20 -0300

    Nível 03: Implementação de Retry e DQL para tentar enviar os dados ao remetente e caso após 3 tentativas ele não responda,salvamos em um tópico de mensagens mortas (DQL), garantindo a integridade dos dados caso o banco caia.

---
+++ src/main/java/br/com/messaging/messaging_springboot/config/KafkaConfig.java
@@ -0,0 +1,35 @@
+package br.com.messaging.messaging_springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+/**
+ * Classe criada para tratarmos os eventos mandados p DQL (um termo antigo dos correios que enviava uma carta ao remetente,
+ * e caso ele estivesse morto ou o endereço estivesse errado, ele deixava "armazenado" para os dados não se perderem ate o
+ * responsavel se manifestar)
+ */
+
+@Configuration
+public class KafkaConfig {
+
+    // Essa configuração define: "Se der erro, tente de novo. Se falhar tudo, mande pra DLQ"
+    //se o banco falhar ele tenta 3x, se nao funcionar ele manda para um topico de mensagens mortas
+    @Bean
+    public DefaultErrorHandler errorHandler(KafkaTemplate<Object, Object> template) {
+
+        // DLQ Recoverer recebe a mensagem morta
+        // Ele vai mandar para o tópico original + ".DLQ" (ex: hello-topic.DLQ)
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(template);
+
+        // Regra de Tentativas (Backoff)
+        // Tente 3 vezes, esperando 1 segundo (1000ms) entre cada tentativa
+        FixedBackOff backOff = new FixedBackOff(1000L, 3);
+
+        //manipulador de erros oficial
+        return new DefaultErrorHandler(recoverer, backOff);
+    }
+}
